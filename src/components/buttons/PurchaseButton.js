653fb9c646d8b499462d662e2ca9f5d253dd44e3
Siddharth Vijay <siddharth.vijay333@gmail.com>
2021-06-21 13:40:38 -0400

    Lecture 8: CSS Selectors and FIlter Effect

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -34,13 +34,21 @@ const Wrapper = styled.h1`
   grid-template-columns: 53px auto;
   align-items: center;
   gap: 20px;
-  transition: 0.8s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &,
+  * {
+    transition: 0.8s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
 
   :hover {
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
       0px 30px 60px rgba(23, 0, 102, 0.5),
       inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
     transform: translateY(-3px);
+
+    .icon {
+      transform: scale(1.2);
+    }
   }
 `
 
@@ -66,6 +74,9 @@ const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
 
 const IconWrapper = styled.div`
@@ -79,4 +90,8 @@ const IconWrapper = styled.div`
   justify-content: center;
   justify-self: center;
   position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(150%) saturate(120%);
+  }
 `
