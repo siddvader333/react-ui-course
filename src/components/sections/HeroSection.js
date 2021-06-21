4bd209c1f0e4206c39f5ead83a680fba3bbd1af8
Siddharth Vijay <siddharth.vijay333@gmail.com>
2021-06-21 13:54:40 -0400

    Lecture 9: Backdrop-filter Blur

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import PurchaseButton from "../buttons/PurchaseButton"
+import MockupAnimation from "../animations/MockupAnimation"
 const HeroSection = () => {
   return (
     <Wrapper>
@@ -22,6 +23,7 @@ const HeroSection = () => {
             subtitle="120+ hours of video"
           />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -30,12 +32,15 @@ export default HeroSection
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
