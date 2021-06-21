2b16222d57905f6d72df7ffbb9b18e15d72765b8
Siddharth Vijay <siddharth.vijay333@gmail.com>
2021-06-21 15:40:32 -0400

    Lecture 12: Grid Repeat and Data

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,27 +1,56 @@
 import React from "react"
 import { Link } from "gatsby"
-
-const menuData = [
-  { title: "courses", icon: "/images/icons/courses.svg", link: "/courses" },
-  {
-    title: "Tutorials",
-    icon: "/images/icons/tutorials.svg",
-    link: "/tutorials",
-  },
-  { title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
-]
+import styled from "styled-components"
+import { menuData } from "../../data/menuData"
 
 const Header = () => {
   return (
-    <>
-      {menuData.map((item, index) => (
-        <Link key={index} to={item.link}>
-          <img src={item.icon} alt={item.title} />
-          {item.title}
-        </Link>
-      ))}
-    </>
+    <Wrapper>
+      <img src="/images/logos/logo.svg" />
+      <MenuWrapper>
+        {menuData.map((item, index) => (
+          <Link key={index} to={item.link}>
+            <MenuItem>
+              <img src={item.icon} alt={item.title} />
+              {item.title}
+            </MenuItem>
+          </Link>
+        ))}
+      </MenuWrapper>
+    </Wrapper>
   )
 }
 
 export default Header
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  display: grid;
+  grid-template-columns: 44px auto;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 30px;
+  align-items: center;
+`
+const MenuWrapper = styled.div`
+  display: grid;
+  gap: 30px;
+  grid-template-columns: repeat(5, auto);
+`
+const MenuItem = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  display: grid;
+  grid-template-columns: 24px auto;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  transition: 0.5s ease-out;
+  border-radius: 10px;
+
+  :hover {
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+  }
+`
